2831ea1266ca8876b606fdf84d2487f9055168c7
reinhardjk14 <reinhardjk14@gmail.com>
2022-05-25 20:42:51 +0700

    penambahan object list buku

---
+++ script.js
@@ -0,0 +1,152 @@
+const BOOKS_AVALAIBLE = [
+  {
+    name: 'The Power Of Focus',
+    category: 'Self-Development',
+    price: 73000,
+    stock: 10
+  },
+  {
+    name: 'Sebuah Seni Untuk Bersikap Bodo Amat',
+    category: 'Self-Development',
+    price: 78000,
+    stock: 10
+  },
+  {
+    name: 'Insecurity Is My Middle Name',
+    category: 'Self-Development',
+    price: 99000,
+    stock: 10
+  },
+  {
+    name: 'Talking To Strangers',
+    category: 'Self-Development',
+    price: 119000,
+    stock: 10
+  }, 
+  {
+    name: 'Ego Is The Enemy',
+    category: 'Self-Development',
+    price: 98000,
+    stock: 10
+  },
+  {
+    name: 'Detektif Conan 100',
+    category: 'Comic',
+    price: 50000,
+    stock: 10
+  },
+  {
+    name: 'Spy x Family 02',
+    category: 'Comic',
+    price: 45000,
+    stock: 10
+  },
+  {
+    name: 'DEMON SLAYER: Kimetsu no Yaiba 08',
+    category: 'Comic',
+    price: 40000,
+    stock: 10
+  }, 
+  {
+    name: 'LC: Kanojo Okarishimasu 07',
+    category: 'Comic',
+    price: 40000,
+    stock: 10
+  },
+  {
+    name: 'Hai, Miiko! 33 - Reguler',
+    category: 'Comic',
+    price: 22000,
+    stock: 10
+  },
+  {
+    name: 'The Baron In The Trees',
+    category: 'Fantasy',
+    price: 153000,
+    stock: 10
+  }, 
+  {
+    name: 'Everna: Rajni Sari',
+    category: 'Fantasy',
+    price: 55000,
+    stock: 10
+  }, 
+  {
+    name: 'The Story Of Kullervo',
+    category: 'Fantasy',
+    price: 127000,
+    stock: 10
+  }, 
+  {
+    name: 'Sleeping Giants',
+    category: 'Fantasy',
+    price: 88000,
+    stock: 10
+  },
+  {
+    name: 'Bannerless',
+    category: 'Fantasy',
+    price: 144000,
+    stock: 10
+  },
+  {
+    name: 'The Cellar',
+    category: 'Horror',
+    price: 181000,
+    stock: 10
+  }, 
+  {
+    name: 'Deep and Dark and Dangerous',
+    category: 'Horror',
+    price: 143000,
+    stock: 10
+  },
+  {
+    name: 'Time for Andrew',
+    category: 'Horror',
+    price: 87000,
+    stock: 10
+  },
+  {
+    name: 'Passing Strange',
+    category: 'Horror',
+    price: 148000,
+    stock: 10
+  },
+  {
+    name: 'Creepy Case Club 5: Kasus Jendela Siluman',
+    category: 'Horror',
+    price: 48000,
+    stock: 10
+  },
+  {
+    name: 'First or Fake Love',
+    category: 'Romance',
+    price: 76000,
+    stock: 10
+  },
+  {
+    name: 'Le Mariage: Medium Rare Mom',
+    category: 'Romance',
+    price: 77000,
+    stock: 10
+  },
+  {
+    name: 'Leka Lara',
+    category: 'Romance',
+    price: 27000,
+    stock: 10
+  },
+  {
+    name: 'MetroPop: Oh, My Baby Blue',
+    category: 'Romance',
+    price: 89000,
+    stock: 10
+  },
+  {
+    name: 'Young Adult: The Arson Project',
+    category: 'Romance',
+    price: 86000,
+    stock: 10
+  }
+]
